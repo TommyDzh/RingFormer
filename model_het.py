aa9292426a64f60c44ed6ba3e9f9d781d4e2d3fa
TommyDzh <43538513+TommyDzh@users.noreply.github.com>
2024-08-20 11:30:01 +0800

    Update model_het.py

--- model_het.py
+++ model_het.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 import copy
-from ogb.graphproppred.mol_encoder import AtomEncoder, BondEncoder
+from ogb.graphproppred.mol_encoder import AtomEncoder
 from torch_scatter import scatter_mean
 from layer_het import *
 import sklearn.covariance
@@ -27,6 +27,28 @@ from torch_geometric.nn import Linear
 # SSL
 import GCL.losses as L
 from GCL.models import DualBranchContrast
+
+class BondEncoder(torch.nn.Module):
+    
+    def __init__(self, emb_dim):
+        super(BondEncoder, self).__init__()
+        
+        self.bond_embedding_list = torch.nn.ModuleList()
+        full_bond_feature_dims = [22, 6, 2]
+        for i, dim in enumerate(full_bond_feature_dims):
+            emb = torch.nn.Embedding(dim, emb_dim)
+            torch.nn.init.xavier_uniform_(emb.weight.data)
+            self.bond_embedding_list.append(emb)
+
+    def forward(self, edge_attr):
+        bond_embedding = 0
+        for i in range(edge_attr.shape[1]):
+            bond_embedding += self.bond_embedding_list[i](edge_attr[:,i])
+
+        return bond_embedding   
+
+
+
 
 class RingEncoder(torch.nn.Module):
 
